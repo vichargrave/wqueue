7f40fa85f13d8aa2d2c0541645f0f197d81dae8c
vichargrave <vichargrave@gmail.com>
2013-03-08 15:55:13 -0800

    Checked in the wrong main.cpp last time.  This is the one from the article.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
    main.cpp
 
-   Test application for a Java style thread class in C++.
+   Test application for the multithreaded work queue in C++.
   
    ------------------------------------------
 
@@ -22,30 +22,79 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string>
 #include "thread.h"
-
-class MyThread : public Thread
+#include "wqueue.h"
+ 
+class WorkItem
 {
-    public:
-    void *run() {
-        for (int i = 0; i < 5; i++) {
-            printf("thread %lu running - %d\n",  (long unsigned int)self(), i+1);
-            sleep(2);
+    string m_message;
+    int    m_number;
+ 
+  public:
+    WorkItem(const char* message, int number) 
+          : m_message(message), m_number(number) {}
+    ~WorkItem() {}
+ 
+    const char* getMessage() { return m_message.c_str(); }
+    int getNumber() { return m_number; }
+};
+
+class ConsumerThread : public Thread
+{
+    wqueue<WorkItem*>& m_queue;
+ 
+  public:
+    ConsumerThread(wqueue<WorkItem*>& queue) : m_queue(queue) {}
+ 
+    void* run() {
+        // Remove 1 item and process it. Blocks if no items are 
+        // available to process.
+        for (int i = 0;; i++) {
+            printf("thread %lu, loop %d - waiting for item...\n", 
+                  (long unsigned int)self(), i);
+            WorkItem* item = (WorkItem*)m_queue.remove();
+            printf("thread %lu, loop %d - got one item\n", 
+                  (long unsigned int)self(), i);
+            printf("thread %lu, loop %d - item: message - %s, number - %d\n", 
+                  (long unsigned int)self(), i, item->getMessage(), 
+                   item->getNumber());
+            delete item;
         }
-        printf("thread done %lu\n", (long unsigned int)self());
         return NULL;
     }
 };
 
 int main(int argc, char** argv)
 {
-    MyThread* thread1 = new MyThread();
-    MyThread* thread2 = new MyThread();
+    // Process command line arguments
+    if ( argc != 2 ) {
+        printf("usage: %s <iterations>\n", argv[0]);
+        exit(-1);
+    }
+    int iterations = atoi(argv[1]);
+ 
+    // Create the queue and worker threads
+    wqueue<WorkItem*>  queue;
+    ConsumerThread* thread1 = new ConsumerThread(queue);
+    ConsumerThread* thread2 = new ConsumerThread(queue);
     thread1->start();
     thread2->start();
-    thread1->join();
-    thread2->join();
-    printf("main done\n");
+ 
+    // Add items to the queue
+    WorkItem* item;
+    for (int i = 0; i < iterations; i++) {
+        item = new WorkItem("abc", 123);
+        queue.add(item);
+        item = new WorkItem("def", 456);
+        queue.add(item);
+        item = new WorkItem("ghi", 789);
+        queue.add(item);
+        sleep(1);
+    }
+ 
+    // Wait for the queue to be empty
+    while (queue.size() < 0);
+    printf("done\n");
     exit(0);
 }
